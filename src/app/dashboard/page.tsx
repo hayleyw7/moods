473cddff605de3109abe8d09f72435d870763d2f
Hayley Witherell <hayleywitherell@gmail.com>
2023-12-22 15:32:01 -0700

    Add loading page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,22 @@
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 
+import React, { Suspense } from 'react';
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+// import { usePathname } from 'next/navigation'
+import Loading from './loading'
 
 export default function Page() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Suspense fallback={<Loading />}>
+        <h1>Dashboard</h1>
         <li>
           <Link href="/">Home</Link>
         </li>
         <li>
           <Link href="/dashboard#settings">Settings</Link>
         </li>
+      </Suspense>
     </div>
   )
 }
